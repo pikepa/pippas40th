a9521680ecce1e0d013c1bd83ae7e663526cbaf4
Peter Pike <pikepeter@gmail.com>
2020-06-27 07:24:03 +0800

    added temp background image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+<div>
 
-<div  class="flex flex-col items-center ">
-    <div>
-        <H1 class="text-pink text-6xl my-8">Pippa's 40 on 28th May 2021!</H1>
-    </div>
-    <div class="w-1/2">
-        <x-input.centered.text />
-    </div>
-    <div class="w-1/2">
-        <livewire:register.emailcapture />
+    <div class="flex flex-col items-center ">
+        <div class="bg-gray-400 rounded-lg p-2 bg-opacity-50">
+
+            <H1 class="text-pink text-6xl ">Pippa's 40 on 28th May 2021!</H1>
+        </div>
+        <div class="w-1/2 bg-gray-400 rounded-lg p-2 m-4 bg-opacity-50">
+            <x-input.centered.text />
+        </div>
+        <div class="w-1/2">
+            <livewire:register.emailcapture />
+        </div>
     </div>
 </div>
 @endsection
-
-
