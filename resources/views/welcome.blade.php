32c5858cb3bab9ecc76ac6ebb3844a10e4f104a9
Peter Pike <pikepeter@gmail.com>
2020-06-27 13:58:15 +0800

    minor Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
 <div class="bg-white bg-opacity-50 w-full h-screen pt-8">
 
     <div class="flex flex-col items-center space-y-6 pt-8">
-        <div class="text-center">
-            <H1 class="text-pink text-semibold text-6xl ">Pippa's 40 on 28th May 2021!</H1>
+
+        <div>
+            <H1 class="text-center text-pink text-6xl ">Pippa's 40 on 28th May 2021!</H1>
         </div>
         <div class="sm:w-1/2 ">
             <x-input.centered.text />
