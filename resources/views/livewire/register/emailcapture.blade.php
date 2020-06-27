7a00ef6e88de620c396515cd1b7e1beb475edb16
Peter Pike <pikepeter@gmail.com>
2020-06-27 09:40:23 +0800

    minor Updates

--- resources/views/livewire/register/emailcapture.blade.php
+++ resources/views/livewire/register/emailcapture.blade.php
@@ -5,7 +5,7 @@
         <div class="mt-1 relative rounded-md shadow-sm">
 
             <div>
-                <input wire:model="email" id="email" name="email" class="form-input block w-full pl-10 sm:text-sm sm:leading-5" placeholder="you@example.com" />
+                <input wire:model="email" id="email" name="email" class="form-input block w-full pl-4 sm:text-sm sm:leading-5" placeholder="you@example.com" />
 
             </div>
             <div class="mt-2 text-red-600">
@@ -16,7 +16,7 @@
         <div class="mt-6">
             <span class=" rounded-md shadow-sm">
                 <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent 
-                text-sm font-medium rounded-md  bg-pink hover:bg-purple-700 text-gray-700 focus:outline-none focus:border-indigo-700">
+                text-sm font-medium rounded-md  bg-pink hover:bg-red-500 text-gray-700 focus:outline-none focus:border-indigo-700">
                     Register
                 </button>
             </span>
