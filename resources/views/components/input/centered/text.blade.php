3088e7fe78e909ec7263bd3944a677d08ba79dfe
Peter Pike <pikepeter@gmail.com>
2020-06-28 12:22:02 +0800

    minor Updates

--- resources/views/components/input/centered/text.blade.php
+++ resources/views/components/input/centered/text.blade.php
@@ -1,10 +1,14 @@
 <div class="font-semibold text-center space-y-4 m-4">
     <p class="text-bold">Pippa’s turning 40 and we all know she likes a good party</p>
-    <p> Please join us as we celebrate with our <span class="text-pink">Fit, Fast and Fabulous</span> Hostess on the beautiful</p>
+    <p> Please join us as we celebrate with our</p>
+     <p class="text-pink">Fit, Fast and Fabulous Hostess</p> 
+     <p>on the beautiful</p>
     <div>
-        <H1 class="text-pink text-6xl my-8">Hamilton Island, Qld</H1>
+        <H1 class="text-pink text-6xl my-6">Hamilton Island, Qld</H1>
     </div>
-    <p>A program of events is currently in the making for the 25th to 29th May so join us for all that you can. Details coming soon…</p>
+    <p>A program of events is currently in the making for the </p>
+    <p>25th to 29th May,</p>
+        <p>so join us for all that you can. Details coming soon…</p>
     <p class="text-sm font-semibold">(We realise this will be subject to international travel restrictions so are pencilling in a second date in late October and will follow up to confirm the date as we approach the new year.)</p>
     <p>Please register so we can keep you up to date as plans unfold.</p>
 </div>
