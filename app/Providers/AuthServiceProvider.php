a1312067e0e13888acabf406bc78a994e2c96cb1
Peter Pike <pikepeter@gmail.com>
2020-07-05 15:44:37 +0800

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::define('viewMailcoach', function ($user = null) {
+            return optional($user)->admin;
+        });
+
         //
     }
 }
